cebadc515cd3288d3e790ca6018abee6c353f452
wdong20 <wdong20@stuy.edu>
2020-11-03 14:18:34 -0500

    Created and Tested: SuperArray size().

--- Tester.java
+++ Tester.java
@@ -1,5 +1,10 @@
 public class Tester {
     public static void main(String[] abcdefghijklmnopqrstuvwxyz) {
+        SuperArray list = new SuperArray();
+
+        System.out.println("Should all be true:");
         
+        System.out.println("size method:");
+        System.out.println(list.size() == 0);
     }
 }
