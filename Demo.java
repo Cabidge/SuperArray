7cb3d51baf8a4a97c8d9c26b0c99a8015b1c95b4
wdong20 <wdong20@stuy.edu>
2020-11-06 11:41:10 -0500

    Created and Tested: Demo removeDuplicates().

--- Demo.java
+++ Demo.java
@@ -1,5 +1,21 @@
 public class Demo {
     public static void main(String[] a) {
-        
+        SuperArray words = new SuperArray();
+        //grouped to save vertical space
+        words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");     
+        words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
+        words.add("una");    words.add("ebi");     words.add("toro"); 
+
+        System.out.println(words);
+        removeDuplicates(words);
+        System.out.println(words);
+    }
+
+    public static void removeDuplicates(SuperArray s) {
+        for (int i = s.size() - 1; i > 0; i--) {
+            if (s.indexOf(s.get(i)) != i) {
+                s.remove(i);
+            }
+        }
     }
 }
